3b8cffb4851c66a5e94e3fbc398d28e66906031a
leogsousa <leonardogomessousa5@gmail.com>
2020-06-07 20:18:43 -0300

    Feito o projeto da semana 3 blackjack completo com desafios imcompletos

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -11,23 +11,29 @@
  * 
  */
 
- console.log("Bem vindo ao jogo de Blackjack!")
+console.log("Bem vindo ao jogo de Blackjack!")
 
- if(confirm("Quer iniciar uma nova rodada?")) {
-	const carta = comprarCarta(); 
-   // Sorteia uma carta. Por exemplo, o rei de ouros
+if(confirm("Quer iniciar uma nova rodada?")) {
+   const carta1Usuario = comprarCarta(); 
+   const carta2Usuario = comprarCarta(); 
+   const carta1Computador = comprarCarta(); 
+   const carta2Computador = comprarCarta(); 
+   let pontuacaoUsuario = 0
+   let pontuacaoComputador = 0
 
-   console.log(carta.texto) 
-   // imprime o texto da carta. Nesse caso: "K♦️"
-   
-   comprarCarta();
-   
-   console.log(carta.valor) 
-   // imprime o valor da carta (um número). Nesse caso: 10)
-   
-   console.log("Usuário - cartas: " + carta.valor , carta.texto + "♣️  - pontuação 20")
-   console.log("Computador - cartas: " + carta.valor , carta.texto + "  - pontuação 20")
+   pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+   pontuacaoComputador = carta1Computador.valor + carta2Computador.valor
 
+   console.log("Usuário - cartas: " + carta1Usuario.texto , carta2Usuario.texto + " - pontuação " + pontuacaoUsuario)
+   console.log("Usuário - cartas: " + carta1Computador.texto , carta2Computador.texto + " - pontuação " + pontuacaoComputador)
+
+   if(pontuacaoUsuario > pontuacaoComputador){
+      console.log("O usuário ganhou!")
+   }else if(pontuacaoComputador > pontuacaoUsuario){
+      console.log("O computador ganhou!")
+   }else{
+      console.log("Empate!")
+   }
 } else {
 	console.log("O jogo acabou")
 }
