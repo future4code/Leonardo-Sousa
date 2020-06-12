71206790688c5c7db281d06d1fd7029c4706bba5
leogsousa <leonardogomessousa5@gmail.com>
2020-06-12 13:28:03 -0300

    Feito o exercício de interpretação de código 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,10 @@
+// Interpretação de código - Exercício 1 ------------------------------------------------------------
+
+// Cria uma função com o nome conversorMoeda com o parâmetro valorEmDolar,
+// dentro dela é criado um prompt para receber a cotação do dolar,
+// vai retornar uma string concatenada no calculo para saber o valor em reais que é o parametro da função
+// multiplicado pela cotação, é criada uma variável meuDinheiro que vai receber a função conversorMoeda
+// com atributo 100 que é o valorEmDolar. No console é mostrado o valor em reais equivalente a 100 dolares
+// que esta na variavel meuDinheiro que recebe o return da função. 
+
+
