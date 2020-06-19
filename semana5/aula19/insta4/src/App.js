079468dde48061220820f7d17173516e872aaf6c
leogsousa <leonardogomessousa5@gmail.com>
2020-06-19 00:33:05 -0300

    Feito o exercício do dia completo/ insta4, form controlado e styled-components

--- semana5/aula19/insta4/src/App.js
+++ semana5/aula19/insta4/src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components'
+
+const Input = styled.input `
+  padding: 0;
+  width: 300px;
+`
+const FormPost = styled.div `
+  width: 306px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px auto;
+  background-color: rgb(224, 9, 110);
+  > *{
+    margin: 2px;
+  }
+  
+`
+const Button = styled.button `
+  width: 100px;
+  border-radius: 4px;
+  border: none;
+`
+
 
 class App extends React.Component {
   state = {
@@ -20,8 +43,42 @@ class App extends React.Component {
         fotoUsuario: "https://picsum.photos/50/50?random=3",
         fotoPost: "https://picsum.photos/200/150?random=3"
       }
-    ]
+    ],
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
   }
+
+   adicionaPost = () => {
+    const novoPost ={
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      
+      fotoUsuario: this.state.valorInputFotoUsuario,
+
+      fotoPost: this.state.valorInputFotoPost
+    }
+
+    const novoArray = [...this.state.user, novoPost];
+    
+    this.setState({ 
+      user: novoArray, 
+      valorInputNomeUsuario: "",
+      valorInputFotoPost: "",
+      valorInputFotoUsuario: ""
+    });
+   }
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+  };
+
+  onChangeInputFotoDePerfil = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoDoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
   
   render() {
 
@@ -35,8 +92,28 @@ class App extends React.Component {
       );
     });
     return (
-      <div className={'app-container'}>
-        {listaDeUsuarios}
+      <div>
+        <FormPost>
+          <Input
+            value={this.state.valorInputNomeUsuario}
+            onChange={this.onChangeInputNomeUsuario}
+            placeholder={"Nome de Usuario"}
+          />
+          <Input
+            value={this.state.valorInputFotoUsuario}
+            onChange={this.onChangeInputFotoDePerfil}
+            placeholder={"Link foto de perfil"}
+          />
+          <Input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoDoPost}
+            placeholder={"Link foto do post"}
+          />
+          <Button onClick={this.adicionaPost}>Adicionar</Button>
+        </FormPost>
+        <div className={'app-container'}>
+          {listaDeUsuarios}
+        </div>
       </div>
     );
   }
