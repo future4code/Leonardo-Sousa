0911502b4b6f97094856fd3e9846ef2a135f5030
leogsousa <leonardogomessousa5@gmail.com>
2020-07-02 18:15:57 -0300

    Feito o projeto do dia completo sem desafios / aula 27

--- semana7/aula27/harry-potter-api/src/App.js
+++ semana7/aula27/harry-potter-api/src/App.js
@@ -3,56 +3,53 @@ import styled from "styled-components";
 import axios from "axios";
 
 const Container = styled.div`
-  font-family: sans-serif;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #AB4D2E;
+`
+const BotaoSorteio = styled.button`
+  background-color: #AB953C;
+  border: none;
+  border-radius: 2px;
+  min-width: 150px;
+  min-height: 24px;
+`
+const House = styled.div`
+  color: white;
+  font-size: 24px;
 `
 
 export class App extends React.Component {
   state = {
-    personagens: [],
-    imagemPersonagem: ""
+    casa: ""
   }
   
   key = "$2a$10$GMwdzTNjxWQ3ZbaZxdXZ8O50lSlkcpJqBuEkCYq64f2muhDpYQ4OC"
 
-  componentDidMount = () => {
-    this.pegaPersonagens();
-  };
-
-  pegaPersonagens = () => {
-    axios.get(`https://www.potterapi.com/v1/characters?key=${this.key}`)
+  sorteiaCasa = () => {
+    axios.get(`https://www.potterapi.com/v1/sortingHat?key=${this.key}`)
     .then(response => {
-      this.setState({ personagens: response.data });
+      this.setState({ casa: response.data + "!"});
     })
     .catch(err => {
       console.log(err.message);
     })
   }
-
  
 
   render(){
     return (
       <Container>
-        <select >
-          <option value={""} />
-          {this.state.personagens.map(personagem => {
-            return (
-              <option key={personagem.name} value={personagem.name}>
-                {" "}
-                {personagem.name}
-              </option>
-            );
-          })}
-        </select>
-        
+        <BotaoSorteio onClick= {this.sorteiaCasa}>Sortear Casa</BotaoSorteio>
+        <House>{this.state.casa}</House>
       </Container>
     );
   }
 }
 export default App;
-
-// -------------------------------------------------------------------------------
 
 
 
