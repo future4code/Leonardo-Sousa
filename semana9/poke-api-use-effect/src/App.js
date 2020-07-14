3ea8883b43956912791bf96816bf7e551e99ed99
leogsousa <leonardogomessousa5@gmail.com>
2020-07-14 17:10:43 -0300

    Feito o exercício do dia useEffect, aula 32

---
+++ semana9/poke-api-use-effect/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState , useEffect} from "react";
+import axios from "axios";
+import PokeCard from "./components/PokeCard";
+
+function App() {
+  const [pokeList, alteraPokeList] = useState([])
+  const [pokeName, alteraPokeName] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        alteraPokeList(response.data.results);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const changePokeName = event => {
+    alteraPokeName(event.target.value)
+  }
+ 
+  return (
+    <div>
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </div>
+  );
+}
+
+export default App;
